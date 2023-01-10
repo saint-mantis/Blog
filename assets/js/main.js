a0cc0c3b755ebd658f0d78cbde5028c810293044
Arun Babu <59362586+saint-mantis@users.noreply.github.com>
2023-01-10 16:23:19 +0530

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,8 @@ function showMenu() {
         document.getElementById("mb-navbox").style.visibility = "visible";
         document.getElementById("mb-navbox").style.width = "100%";
         document.getElementById("mb-navbox").style.height = "300px";
+        document.getElementById("mb-navbox").style.transition = "height 0.5s";
+
         count = 1;
     } 
     else {
@@ -19,11 +21,14 @@ function showMenu() {
             document.getElementById("mb-navbox").style.visibility = "hidden";
             document.getElementById("mb-navbox").style.width = "0px";
             document.getElementById("mb-navbox").style.height = "0px";
+            document.getElementById("mb-navbox").style.transition = "height 0.5s";
         }
         else {
             document.getElementById("mb-navbox").style.visibility = "visible";
             document.getElementById("mb-navbox").style.width = "100%";
             document.getElementById("mb-navbox").style.height = "300px";
+            document.getElementById("mb-navbox").style.transition = "height 0.5s";
+
         }
     }
 
